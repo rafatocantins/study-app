3d544f1cdce1f6ffdcfc0c887e999c284b89c3c4
rafatocantins <rafewebdev@gmail.com>
2019-10-16 14:42:55 +0100

    add functionality to my first app

--- App.js
+++ App.js
@@ -1,19 +1,74 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity ,StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  // use useState to get the values 
+  // enteredNote is the value and setEnteredNote is the action 'caused by React.dispatch'
+  const [ enteredNote, setEnteredNote ] = useState('');
+  const [ notes, setNotes ] = useState([]);
+  // create an handler 
+  const noteInputHandler = (enteredText) => {
+    setEnteredNote(enteredText);
+  };
+
+  const addNoteHandler = () => {
+    setNotes( currentNote => ([ ...currentNote, enteredNote ]) )
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.top}>
+        <TextInput 
+          placeholder="Write a note"
+          style={styles.input}
+          onChangeText={noteInputHandler}
+          value={enteredNote}
+        />
+        <TouchableOpacity onPress={addNoteHandler}>
+          <Text style={styles.button}>Add</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        {notes.map((note, id) => 
+          <View key={id} style={styles.notes}>
+            <Text>{note}</Text>
+          </View>
+        )}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
-});
+  top: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center'
+  },
+  input: {
+    borderBottomColor: 'orange',
+    borderBottomWidth: 2,
+    marginBottom: 5,
+    width: '80%'
+  },
+  button: {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontWeight: 'bold',
+    padding: 10,
+    borderRadius: 5,
+  },
+  notes: {
+    alignContent: 'center',
+    width: '100%',
+    backgroundColor: 'orange',
+    color: 'white',
+    marginTop: 10,
+    padding: 12,
+    borderRadius: 5,
+  }
+})
+
